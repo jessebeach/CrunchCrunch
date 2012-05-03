d741d835cce3bb07c38e4a6e6c9811b348cd2cab
J. Renée Beach <splendidnoise@gmail.com>
2012-05-03 15:04:49 -0400

    Initial commit of the calculator framework.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

---
+++ app/config.js
@@ -0,0 +1,13 @@
+// Set the require.js configuration for your application.
+require.config({
+  // Initialize the application with the main application file
+  deps: ["main"],
+
+  paths: {
+    // JavaScript folders
+    libs: "../assets/js/libs",
+
+    // Libraries
+    jquery: "../assets/js/libs/jquery",
+  }
+});
---
+++ app/main.js
@@ -0,0 +1,12 @@
+require([
+  // Libs
+  "jquery",
+
+  // Modules
+],
+
+function($) {
+  $(document).on("click", "a", function (evt) {
+  	console.log(evt);
+	});
+});
